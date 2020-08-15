c9735fb9dabda21d7d3607e1d29c908fd66f117f
William Tambe <tambewilliam@gmail.com>
2020-08-15 11:50:26 -0500

    Target PU32 support

---
+++ arch/pu32/include/uapi/asm/bitsperlong.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
+// (c) William Fonkou Tambe
+
+#ifndef __UAPI_ASM_PU32_BITSPERLONG_H
+#define __UAPI_ASM_PU32_BITSPERLONG_H
+
+# define __BITS_PER_LONG (__SIZEOF_POINTER__ * 8)
+
+#endif /* __UAPI_ASM_PU32_BITSPERLONG_H */
+
