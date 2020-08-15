a3e3e944508f5b26c4f5d0716ad671202e08e1bd
William Tambe <tambewilliam@gmail.com>
2020-08-15 11:50:26 -0500

    Target PU32 support

---
+++ arch/pu32/include/asm/setup.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// (c) William Fonkou Tambe
+
+#ifndef __ASM_PU32_SETUP_H
+#define __ASM_PU32_SETUP_H
+
+#include <uapi/asm/setup.h>
+
+#ifndef __ASSEMBLY__
+
+#endif /* !__ASSEMBLY__ */
+#endif /* __ASM_PU32_SETUP_H */
--- init/main.c
+++ init/main.c
@@ -1441,7 +1441,7 @@ static inline void mark_readonly(void)
 #else
 static inline void mark_readonly(void)
 {
-	pr_warn("This architecture does not have kernel memory protection.\n");
+	//pr_warn("This architecture does not have kernel memory protection.\n");
 }
 #endif
 
