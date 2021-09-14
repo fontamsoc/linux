2a59d615cf5ada8ba6525786fe9c98de94c92f5b
William Tambe <tambewilliam@gmail.com>
2021-09-14 07:51:26 -0500

    bugfix ptrace.c and rework pu32hang()

--- arch/pu32/kernel/hang.process.c
+++ arch/pu32/kernel/hang.process.c
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // (c) William Fonkou Tambe
 
-// Function that must be used instead of
-// panic() within pu32ctxswitchhdlr().
-// TODO: Stop all other cores ...
+// Function that must be used instead of panic() within pu32ctxswitchhdlr().
 void pu32hang (const char *fmt, ...) {
 	va_list args;
 	va_start (args, fmt);
@@ -13,7 +11,5 @@ void pu32hang (const char *fmt, ...) {
 	unsigned long *sp; asm volatile ("setkgpr %0, %%sp" : "=r"(sp));
 	extern void show_stack (struct task_struct *tsk, unsigned long *sp, const char *loglvl);
 	show_stack (NULL, sp, KERN_DEFAULT);
-	// sysret is used after the infinite loop sequence for use
-	// in the debugger, in order to single-step back to userspace.
-	asm volatile ("rli %%sr; j %%sr; sysret":);
+	asm volatile ("rli %%sr; j %%sr":);
 }
--- arch/pu32/kernel/ptrace.c
+++ arch/pu32/kernel/ptrace.c
@@ -12,12 +12,36 @@ int dump_fpu (struct pt_regs *regs, elf_fpregset_t *fpu) {
 }
 
 void show_regs (struct pt_regs *regs, const char *loglvl) {
-	pr_info ("%spc(0x%lx(%pS)) rp(0x%lx(%pS))\n", loglvl,
+	if (regs == (struct pt_regs *)0 || regs == (struct pt_regs *)-1) {
+		// Assume coming from usermode if regs invalid pointer.
+		struct pt_regs uregs;
+		asm volatile ("setkgpr %0, %%sp" : "=r"(uregs.sp));
+		asm volatile ("setkgpr %0, %%1"  : "=r"(uregs.r1));
+		asm volatile ("setkgpr %0, %%2"  : "=r"(uregs.r2));
+		asm volatile ("setkgpr %0, %%3"  : "=r"(uregs.r3));
+		asm volatile ("setkgpr %0, %%4"  : "=r"(uregs.r4));
+		asm volatile ("setkgpr %0, %%5"  : "=r"(uregs.r5));
+		asm volatile ("setkgpr %0, %%6"  : "=r"(uregs.r6));
+		asm volatile ("setkgpr %0, %%7"  : "=r"(uregs.r7));
+		asm volatile ("setkgpr %0, %%8"  : "=r"(uregs.r8));
+		asm volatile ("setkgpr %0, %%9"  : "=r"(uregs.r9));
+		asm volatile ("setkgpr %0, %%10" : "=r"(uregs.r10));
+		asm volatile ("setkgpr %0, %%11" : "=r"(uregs.r11));
+		asm volatile ("setkgpr %0, %%12" : "=r"(uregs.r12));
+		asm volatile ("setkgpr %0, %%sr" : "=r"(uregs.sr));
+		asm volatile ("setkgpr %0, %%fp" : "=r"(uregs.fp));
+		asm volatile ("setkgpr %0, %%rp" : "=r"(uregs.rp));
+		asm volatile ("getuip %0"        : "=r"(uregs.pc));
+		regs = &uregs;
+	}
+	if (!loglvl)
+		loglvl = KERN_INFO;
+	printk ("%spc(0x%lx(%pS)) rp(0x%lx(%pS))\n", loglvl,
 		regs->pc, (void *)regs->pc, regs->rp, (void *)regs->rp);
-	pr_info (
+	printk (
 		"%ssp(0x%lx) r1(0x%lx) r2(0x%lx) r3(0x%lx) r4(0x%lx) r5(0x%lx) r6(0x%lx) r7(0x%lx)\n", loglvl,
 		regs->sp, regs->r1, regs->r2, regs->r3, regs->r4, regs->r5, regs->r6, regs->r7);
-	pr_info (
+	printk (
 		"%sr8(0x%lx) r9(0x%lx) r10(0x%lx) r11(0x%lx) r12(0x%lx) sr(0x%lx) fp(0x%lx)\n", loglvl,
 		regs->r8, regs->r9, regs->r10, regs->r11, regs->r12, regs->sr, regs->fp);
 }
@@ -39,44 +63,20 @@ __attribute__((__noinline__)) static void stacktrace (
 void show_stack (struct task_struct *tsk, unsigned long *sp, const char *loglvl) {
 	if (sp) {
 		// sp is set when show_stack() is called from pu32-kernelmode.
-		struct pt_regs regs;
-		regs.sp = (unsigned long)sp;
-		asm volatile ("cpy %0, %%sp" : "=r"(sp));
 		pr_info ("%sstacktrace:\n", loglvl);
+		asm volatile ("cpy %0, %%sp" : "=r"(sp));
 		stacktrace (sp, (unsigned long *)pu32_stack_bottom(sp), loglvl);
-		sp = (unsigned long *)regs.sp;
-		asm volatile ("setkgpr %0, %%1" : "=r"(regs.r1));
-		asm volatile ("setkgpr %0, %%2" : "=r"(regs.r2));
-		asm volatile ("setkgpr %0, %%3" : "=r"(regs.r3));
-		asm volatile ("setkgpr %0, %%4" : "=r"(regs.r4));
-		asm volatile ("setkgpr %0, %%5" : "=r"(regs.r5));
-		asm volatile ("setkgpr %0, %%6" : "=r"(regs.r6));
-		asm volatile ("setkgpr %0, %%7" : "=r"(regs.r7));
-		asm volatile ("setkgpr %0, %%8" : "=r"(regs.r8));
-		asm volatile ("setkgpr %0, %%9" : "=r"(regs.r9));
-		asm volatile ("setkgpr %0, %%10" : "=r"(regs.r10));
-		asm volatile ("setkgpr %0, %%11" : "=r"(regs.r11));
-		asm volatile ("setkgpr %0, %%12" : "=r"(regs.r12));
-		asm volatile ("setkgpr %0, %%sr" : "=r"(regs.sr));
-		asm volatile ("setkgpr %0, %%fp" : "=r"(regs.fp));
-		asm volatile ("setkgpr %0, %%rp" : "=r"(regs.rp));
-		asm volatile ("getuip %0" : "=r"(regs.pc));
 		pu32FaultReason faultreason;
 		asm volatile ("getfaultreason %0\n" : "=r"(faultreason));
 		unsigned long sysopcode;
 		asm volatile ("getsysopcode %0" : "=r"(sysopcode));
 		pr_info ("%s%s: %s\n", loglvl,
-			pu32faultreasonstr (faultreason, (regs.pc&1)),
+			pu32faultreasonstr (faultreason, 0),
 			pu32sysopcodestr (sysopcode));
-		show_regs(&regs, loglvl);
-	} else
-		asm volatile ("cpy %0, %%sp" : "=r"(sp));
-	// pu32_get_thread_info() is used used to retrieve thread_info
-	// instead-of current_thread_info() because this function can be
-	// called from interrupt context where (sp != current_thread_info()->task->stack)
-	// because interrupt context uses pu32ctxswitchhdlr() dedicated stack.
-	struct thread_info *ti = pu32_get_thread_info(sp);
+	}
+	struct thread_info *ti = current_thread_info();
 	unsigned long ksp = ti->ksp;
+	sp = ti->task->stack;
 	pr_info ("%sstacktrace:\n", loglvl);
 	stacktrace (sp, (unsigned long *)ksp, loglvl);
 	unsigned iskthread = (ti->task->flags&PF_KTHREAD);
