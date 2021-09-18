041d407f983cc45c126c68cf9d71190c3a2a7d08
William Tambe <tambewilliam@gmail.com>
2021-09-18 12:03:59 -0500

    further improve memcpy() such that it tries harder to use uintcpy()

--- arch/pu32/lib/memcpy.c
+++ arch/pu32/lib/memcpy.c
@@ -5,7 +5,7 @@
 #include <linux/export.h>
 #include <linux/types.h>
 
-// Copies cnt u8s in uints from memory area src to memory area dst.
+// Copies cnt bytes in uints from memory area src to memory area dst.
 // The memory areas must not overlap.
 // Returns (dst+(cnt*sizeof(unsigned long))).
 void *uintcpy (void *dst, const void *src, unsigned long cnt); __asm__ (
@@ -16,7 +16,7 @@ void *uintcpy (void *dst, const void *src, unsigned long cnt); __asm__ (
 
 	"rli %sr, 2f\n"
 	"jz %3, %sr\n"
-	"cpy %4, %3\n"
+	"_uintcpy: cpy %4, %3\n"
 	"li8 %5, "__stringify(__SIZEOF_POINTER__)"\n"
 	"modu %4, %5\n"
 	"divu %3, %5\n"
@@ -28,17 +28,20 @@ void *uintcpy (void *dst, const void *src, unsigned long cnt); __asm__ (
 	"inc8 %2, "__stringify(__SIZEOF_POINTER__)"\n"
 	"inc8 %3, -1\n"
 	"jnz %3, %sr\n"
-	"1: cpy %3, %4\n"
-	"rli %sr, u8cpy\n"
-	"jnz %3, %sr\n"
+	"1: jz %4, %rp\n"
+	"cpy %3, %4; li8 %4, 0\n"
+	"rli %sr, _u8cpy\n"
+	"j %sr\n"
 	"2: j %rp\n"
 
 	".size    uintcpy, (. - uintcpy)\n");
 
-// Copies cnt u8s from memory area src to memory area dst.
+// Copies cnt bytes from memory area src to memory area dst.
+// If argument cnt_for_uintcpy is non-null, it is the number of bytes
+// to copy using uintcpy() from (dst+cnt) which must be aligned to uint.
 // The memory areas must not overlap.
-// Returns (dst+cnt).
-void *u8cpy (void *dst, const void *src, unsigned long cnt); __asm__ (
+// Returns (dst+cnt+cnt_for_uintcpy).
+void *u8cpy (void *dst, const void *src, unsigned long cnt, unsigned long cnt_for_uintcpy); __asm__ (
 	".text\n"
 	".type    u8cpy, @function\n"
 	".p2align 1\n"
@@ -46,20 +49,32 @@ void *u8cpy (void *dst, const void *src, unsigned long cnt); __asm__ (
 
 	"rli %sr, 1f\n"
 	"jz %3, %sr\n"
-	"rli %sr, 0f\n"
-	"0: ld8 %4, %2; st8 %4, %1\n"
+	"_u8cpy: rli %sr, 0f\n"
+	"0: ld8 %5, %2; st8 %5, %1\n"
 	"inc8 %1, 1\n"
 	"inc8 %2, 1\n"
 	"inc8 %3, -1\n"
 	"jnz %3, %sr\n"
+	"jz %4, %rp\n"
+	"cpy %3, %4\n"
+	"rli %sr, _uintcpy\n"
+	"j %sr\n"
 	"1: j %rp\n"
 
 	".size    u8cpy, (. - u8cpy)\n");
 
 void *memcpy (void *dest, const void *src, size_t count) {
-	if (((unsigned long)dest|(unsigned long)src)%sizeof(unsigned long))
-		u8cpy (dest, src, count);
-	else
+	if (((unsigned long)dest|(unsigned long)src)%sizeof(unsigned long)) {
+		if (((unsigned long)dest^(unsigned long)src)%sizeof(unsigned long))
+			u8cpy (dest, src, count, 0);
+		else {
+			unsigned long n = (sizeof(unsigned long)-((unsigned long)dest%sizeof(unsigned long)));
+			if (n > count)
+				u8cpy (dest, src, count, 0);
+			else
+				u8cpy (dest, src, n, count-n);
+		}
+	} else
 		uintcpy (dest, src, count);
 	return dest;
 }
