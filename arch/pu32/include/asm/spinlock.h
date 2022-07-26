20e1aa5736591bca176ca3d2f6fb125f934435e3
William Tambe <tambewilliam@gmail.com>
2022-07-26 10:18:46 -0500

    spinlock.h: must not sleep

--- arch/pu32/include/asm/spinlock.h
+++ arch/pu32/include/asm/spinlock.h
@@ -26,13 +26,13 @@ static inline int arch_spin_trylock (arch_spinlock_t *m) {
 
 static inline void arch_spin_lock (arch_spinlock_t *m) {
 	while (xchg(&m->lock[0/*LOCK*/], 1))
-		cpu_relax();
+		/*cpu_relax()*/;
 	while ((m->lock[1/*TICKET*/] - m->lock[2/*CURRENT*/]) == -1)
-		cpu_relax();
+		/*cpu_relax()*/;
 	unsigned long i = m->lock[1/*TICKET*/]++;
 	xchg(&m->lock[0/*LOCK*/], 0); // memory fenced version of: m->lock[0/*LOCK*/] = 0;
 	while (m->lock[2/*CURRENT*/] != i)
-		cpu_relax();
+		/*cpu_relax()*/;
 }
 
 static inline void arch_spin_unlock (arch_spinlock_t *m) {
@@ -60,16 +60,16 @@ static inline int arch_read_trylock (arch_rwlock_t *m) {
 
 static inline void arch_read_lock (arch_rwlock_t *m) {
 	while (xchg(&m->lock[0/*LOCK*/], 1))
-		cpu_relax();
+		/*cpu_relax()*/;
 	if (arch_spin_is_locked((arch_spinlock_t *)m)) {
 		while ((m->lock[1/*TICKET*/] - m->lock[2/*CURRENT*/]) == -1)
-			cpu_relax();
+			/*cpu_relax()*/;
 		unsigned long i = m->lock[1/*TICKET*/]++;
 		xchg(&m->lock[0/*LOCK*/], 0); // memory fenced version of: m->lock[0/*LOCK*/] = 0;
 		while (m->lock[2/*CURRENT*/] != i)
-			cpu_relax();
+			/*cpu_relax()*/;
 		while (xchg(&m->lock[0/*LOCK*/], 1))
-			cpu_relax();
+			/*cpu_relax()*/;
 		xchg(&m->lock[2/*CURRENT*/], (i+1)); // memory fenced version of: ++m->lock[2/*CURRENT*/];
 	}
 	++m->lock[3/*READERCNT*/];
@@ -78,7 +78,7 @@ static inline void arch_read_lock (arch_rwlock_t *m) {
 
 static inline void arch_read_unlock (arch_rwlock_t *m) {
 	while (xchg(&m->lock[0/*LOCK*/], 1))
-		cpu_relax();
+		/*cpu_relax()*/;
 	--m->lock[3/*READERCNT*/];
 	xchg(&m->lock[0/*LOCK*/], 0); // memory fenced version of: m->lock[0/*LOCK*/] = 0;
 }
@@ -96,7 +96,7 @@ static inline int arch_write_trylock (arch_rwlock_t *m) {
 static inline void arch_write_lock (arch_rwlock_t *m) {
 	arch_spin_lock((arch_spinlock_t *)m);
 	while (m->lock[3/*READERCNT*/])
-		cpu_relax();
+		/*cpu_relax()*/;
 }
 
 static inline void arch_write_unlock (arch_rwlock_t *m) {
