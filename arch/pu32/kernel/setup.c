eba771a3c6dd42e24dbb6f3964589d502d1650df
William Tambe <tambewilliam@gmail.com>
2022-07-29 20:46:02 -0500

    pu32: convert to setup_initial_init_mm()

--- arch/pu32/kernel/setup.c
+++ arch/pu32/kernel/setup.c
@@ -228,10 +228,7 @@ void __init setup_smp (void);
 
 void __init setup_arch (char **cmdline_p) {
 
-	init_mm.start_code = (unsigned long)_stext;
-	init_mm.end_code = (unsigned long)_etext;
-	init_mm.end_data = (unsigned long)_edata;
-	init_mm.brk = (unsigned long)_end;
+	setup_initial_init_mm(_stext, _etext, _edata, _end);
 
 	*cmdline_p = boot_command_line;
 
