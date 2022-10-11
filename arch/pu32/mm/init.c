689a7f2f1f91653941d837e29ed65bb13d6850c6
William Tambe <tambewilliam@gmail.com>
2022-10-11 09:59:00 -0500

    pu32/mm: implement VM_GET_PAGE_PROT

--- arch/pu32/mm/init.c
+++ arch/pu32/mm/init.c
@@ -14,3 +14,25 @@ EXPORT_SYMBOL(empty_zero_page);
 void __init mem_init (void) {
 	memblock_free_all();
 }
+
+// Remove W bit on private pages for COW support.
+// Shared pages can have exact HW mapping.
+static const pgprot_t protection_map[16] = {
+	[VM_NONE]                                       = MKP(0, 0, 0),
+	[VM_READ]                                       = MKP(0, 0, 1),
+	[VM_WRITE]                                      = MKP(0, 0, 0), /* COW */
+	[VM_WRITE | VM_READ]                            = MKP(0, 0, 1), /* COW */
+	[VM_EXEC]                                       = MKP(1, 0, 0),
+	[VM_EXEC | VM_READ]                             = MKP(1, 0, 1),
+	[VM_EXEC | VM_WRITE]                            = MKP(1, 0, 0), /* COW */
+	[VM_EXEC | VM_WRITE | VM_READ]                  = MKP(1, 0, 1), /* COW */
+	[VM_SHARED]                                     = MKP(0, 0, 0),
+	[VM_SHARED | VM_READ]                           = MKP(0, 0, 1),
+	[VM_SHARED | VM_WRITE]                          = MKP(0, 1, 0),
+	[VM_SHARED | VM_WRITE | VM_READ]                = MKP(0, 1, 1),
+	[VM_SHARED | VM_EXEC]                           = MKP(1, 0, 0),
+	[VM_SHARED | VM_EXEC | VM_READ]                 = MKP(1, 0, 1),
+	[VM_SHARED | VM_EXEC | VM_WRITE]                = MKP(1, 1, 0),
+	[VM_SHARED | VM_EXEC | VM_WRITE | VM_READ]      = MKP(1, 1, 1)
+};
+DECLARE_VM_GET_PAGE_PROT
