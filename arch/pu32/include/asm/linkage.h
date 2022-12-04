5baf19a534d1d2cc172f4fb5f72a12d8609a13fb
William Tambe <tambewilliam@gmail.com>
2022-12-04 16:58:15 -0600

    implement linkage.h

---
+++ arch/pu32/include/asm/linkage.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// (c) William Fonkou Tambe
+
+#ifndef __ASM_PU32_LINKAGE_H
+#define __ASM_PU32_LINKAGE_H
+
+#define __ALIGN .align 1
+#define __ALIGN_STR ".align 1"
+
+#endif /* __ASM_PU32_LINKAGE_H */
